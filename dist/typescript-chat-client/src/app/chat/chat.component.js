a1d8851225983c403fa5284349c545f1e902b1f1
sdmay10 <sdmay10@gmail.com>
2018-01-25 17:15:50 -0500

    Found error

--- dist/typescript-chat-client/src/app/chat/chat.component.js
+++ dist/typescript-chat-client/src/app/chat/chat.component.js
@@ -8,6 +8,7 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 exports.__esModule = true;
 var core_1 = require("@angular/core");
 var action_1 = require("./shared/model/action");
+var event_1 = require("./shared/model/event");
 var ChatComponent = /** @class */ (function () {
     function ChatComponent(socketService) {
         this.socketService = socketService;
@@ -15,6 +16,51 @@ var ChatComponent = /** @class */ (function () {
         this.messages = [];
     }
     ChatComponent.prototype.ngOnInit = function () {
+    };
+    ChatComponent.prototype.initToConnection = function () {
+        var _this = this;
+        this.socketService.initSocket();
+        this.ioConnection = this.socketService.onMessage()
+            .subscribe(function (message) {
+            _this.messages.push(message);
+        });
+        this.socketService.onEvent(event_1.Event.CONNECT)
+            .subscribe(function () {
+            console.log('CONNECTED');
+        });
+        this.socketService.onEvent(event_1.Event.DISCONNECT)
+            .subscribe(function () {
+            console.log('DISCONNECTED');
+        });
+    };
+    ChatComponent.prototype.sendMessage = function (message) {
+        if (!message) {
+            return;
+        }
+        this.socketService.send({
+            from: this.user,
+            content: message
+        });
+        this.messageContent = null;
+    };
+    ChatComponent.prototype.sendNotification = function (params, action) {
+        var message;
+        if (action === action_1.Action.JOINED) {
+            message = {
+                from: this.user,
+                action: action
+            };
+        }
+        else if (action === action_1.Action.RENAME) {
+            message = {
+                action: action,
+                content: {
+                    username: this.user.name,
+                    previousUsername: params.previousUsername
+                }
+            };
+        }
+        this.socketService.send(message);
     };
     ChatComponent = __decorate([
         core_1.Component({
--- typescript-chat-client/src/app/chat/chat.component.ts
+++ typescript-chat-client/src/app/chat/chat.component.ts
@@ -19,7 +19,53 @@ export class ChatComponent implements OnInit {
 
   constructor(private socketService: SocketService) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+  }
+  private initToConnection(): void {
+  this.socketService.initSocket();
+
+  this.ioConnection = this.socketService.onMessage()
+  .subscribe((message: Message) => {
+    this.messages.push(message);
+  });
+  this.socketService.onEvent(Event.CONNECT)
+    .subscribe(() => {
+        console.log('CONNECTED');
+    });
+  this.socketService.onEvent(Event.DISCONNECT)
+    .subscribe(() => {
+      console.log('DISCONNECTED');
+    })
+  }
+    public sendMessage(message: string): void {
+      if (!message) {
+        return;
+
+      }
+      this.socketService.send({
+        from: this.user,
+        content: message
+      });
+      this.messageContent = null;
+    }
+    public sendNotification(params: any, action: Action): void {
+      let message: Message;
+
+      if (action === Action.JOINED) {
+        message = {
+          from: this.user,
+          action: action
+        }
+      }else if ( action === Action.RENAME) {
+        message = {
+          action: action,
+          content: {
+            username: this.user.name,
+            previousUsername: params.previousUsername
+          }
+        }
+      }
+      this.socketService.send(message);
+    }
   }
 
-}
